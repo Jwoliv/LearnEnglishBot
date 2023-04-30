8c3b472541f0735df4b2f19660d6fcd41828cd5e
Lishchuk Bohdan <bogdanlishchuk04@gmail.com>
2023-04-30 17:42:04 +0300

    Initial commit

---
+++ src/main/java/com/example/LearnEnglishBot/LearnEnglishBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearnEnglishBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnEnglishBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnEnglishBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LearnEnglishBot/LearnEnglishBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearnEnglishBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnEnglishBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
